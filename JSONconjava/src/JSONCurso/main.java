cee50b289fec258bf0552c05433cc8ebd6aef5ac
dsoboy <4g63fp@gmail.com>
2024-10-09 10:24:01 +0200

    up-starWars-json

---
+++ JSONconjava/src/JSONCurso/main.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package JSONCurso;
+
+import java.io.IOException;
+import jsonconjava.Proyecto;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author FP
+ */
+public class main {
+
+    public static void main(String[] args) throws IOException {
+
+        // Crear usuarios
+        UsuarioAvanzado modulo1
+                = new UsuarioAvanzado("Jude Bellingham", 21, 0.0);
+        UsuarioAvanzado modulo2
+                = new UsuarioAvanzado("Kylian Mbappé", 25, 0.0);
+
+        //Crear proyecto con participante
+        Curso curso = new Curso("temporada 24/25 ");
+        curso.agregarModulo(modulo1);
+        curso.agregarModulo(modulo2);
+
+        JSONObject jsonProyecto = new JSONObject();
+        jsonProyecto.put("curso", curso.getNombre());
+
+        JSONArray jsonCursos = new JSONArray();
+        for (UsuarioAvanzado variable : curso.getModulo()) {
+            JSONObject jsonCurso = new JSONObject();
+            jsonCurso.put("nombre", variable.getNombre());
+            jsonCurso.put("horas", variable.getHoras());
+            jsonCurso.put("nota", variable.getNota());
+            jsonCursos.put(jsonCurso);
+
+        }
+        jsonProyecto.put("Participantes", jsonCursos);
+        java.nio.file.Files.write(java.nio.file.Paths.get("proyecto.json"), jsonProyecto.toString().getBytes());
+    }
+}
---
+++ JSONconjava/src/jsonSTW/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jsonSTW;
+
+/**
+ *
+ * @author FP
+ */
+public class Principal {
+    public static void main(String[] args) {
+        
+        film modulo1 = new film
+        ("The Empire Strikes Back","5","It is a dark time","Irvin Kershner");
+        
+        film modulo2 = new film 
+        ("Revenge of the Sith","3","War! The Republic is crumbling","George Lucas");
+            
+        
+        
+        
+    }
+}
